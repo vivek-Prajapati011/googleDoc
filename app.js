4c3fb45c9ddbd2f101dc58916c7a31e911541ab7
vivek prajapati <vivek011prajapati@gmail.com>
2025-04-24 05:10:52 +0530

    starting the server

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ import express from "express"
 const app = express()
 //setting port number
 const port = 3000
+// listening to the port
+app.listen(port, () => {
+console.log("server is started")
+})
