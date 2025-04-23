0c2a0b2582b296a6554421a16236506f5525080e
vivek prajapati <vivek011prajapati@gmail.com>
2025-04-24 05:20:32 +0530

    nothing change here

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const app = express()
 //setting port number
 const port = 3000
 // enabling cores
-app.use((req,res,next) => {
+app.use((req,res, next) => {
     res.set("Acess-Control-Allow-Origin", "*")
 })
+// setting up routes
 // listening to the port
 app.listen(port, () => {
 console.log("server is started")
