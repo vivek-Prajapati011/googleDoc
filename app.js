6361d036a0161b1caceab08a0c67e3eee4cf9829
vivek prajapati <vivek011prajapati@gmail.com>
2025-04-30 05:01:40 +0530

    error handling

--- app.js
+++ app.js
@@ -46,12 +46,14 @@ app.post("/files/*", (req,res) => {
 
 // setting dynamic routes
 app.get("/files/*", (req,res) => { 
-    const dirname = path.join('/', req.params[0]) // getting the file name from the url
+  const filePath = path.join('/', req.params[0]) // getting the file name from the url
     if ( req.query.params === "download") { 
         res.set("Content-Dispositon", "attachment") // setting the content disposition to attchment 
     }
     res.sendFile(`${import.meta.dirname}/storage/${filePath}`, (err) => {
-        res.json({msg : "file not found"})
+        if (err){
+            res.json({msg : "file not found"})
+        }
     }) // sending the file to the client 
 })
 
