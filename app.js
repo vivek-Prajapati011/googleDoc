1d92685b86f5c536a7faef076d693612ec50bc51
vivek prajapati <vivek011prajapati@gmail.com>
2025-05-01 12:33:52 +0530

    transfersing the code to fileRoute.js file

--- app.js
+++ app.js
@@ -24,15 +24,7 @@ app.use((req, res, next) => {
     next()
 })
 
-app.post("/directory/?*", async (res,req) => {
-    const dirname = path.join('/', req.params[0]) 
-    try {
-        await mkdir(`./storage/${dirname}`);
-        res.json({ message: "Directory Created!" });
-      } catch (err) {
-        res.json({ err: err.message });
-      }
-})
+
 
 // creating routes for uploading files 
 app.post("/files/*", (req,res) => { 
