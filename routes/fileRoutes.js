c3dc5d3c2d25598e843f89d880088a90cc2372a9
vivek prajapati <vivek011prajapati@gmail.com>
2025-05-01 12:49:15 +0530

    setting dynamic routes

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -7,3 +7,17 @@ app.post("/files/*", (req,res) => {
         res.json({msg : "file uploaded sucessfully"})
     })
 })
+
+
+// setting dynamic routes
+app.get("/files/*", (req,res) => { 
+  const filePath = path.join('/', req.params[0]) // getting the file name from the url
+    if ( req.query.params === "download") { 
+        res.set("Content-Dispositon", "attachment") // setting the content disposition to attchment 
+    }
+    res.sendFile(`${import.meta.dirname}/storage/${filePath}`, (err) => {
+        if (err){
+            res.json({msg : "file not found"})
+        }
+    }) // sending the file to the client 
+})
