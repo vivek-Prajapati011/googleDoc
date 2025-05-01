770972137c523e38ce1d00c5c137f851c693b359
vivek prajapati <vivek011prajapati@gmail.com>
2025-05-01 12:45:59 +0530

    transfer code to fileRoutes.js file

--- app.js
+++ app.js
@@ -26,15 +26,7 @@ app.use((req, res, next) => {
 
 
 
-// creating routes for uploading files 
-app.post("/files/*", (req,res) => { 
-    const filePath = path.join('/', req.params[0])  
-    const writeStram = createWriteStream(`./storage/${filePath}`)
-    req.pipe(writeStram)
-    req.on("end", () => {
-        res.json({msg : "file uploaded sucessfully"})
-    })
-})
+
   
 
 // setting dynamic routes
