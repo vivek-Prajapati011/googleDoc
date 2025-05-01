b506c7d9a21f84ee9a1ad83c4a9c997f62867820
vivek prajapati <vivek011prajapati@gmail.com>
2025-05-01 12:43:29 +0530

    setting up routes for renaming files and directory

--- routes/directoryRoutes.js
+++ routes/directoryRoutes.js
@@ -7,3 +7,25 @@ app.post("/directory/?*", async (res,req) => {
         res.json({ err: err.message });
       }
 })
+
+
+// setting up routes for renaming files and directory
+app.get ("/directory/?*", async (req, res) => { // optional routing
+    const dirname = path.join('/', req.params[0]) 
+    console.log(dirname)
+    const fullDirpathh = `./storage/${dirname? dirname : ""}`
+   try {
+    const FileList = await readdir(fullDirpathh)
+    // filtering the files and directories
+    const resData = []  
+    for( const items of FileList){ 
+        const stats = await stat(`${fullDirpathh}/${items}`)
+        resData.push({name: items, isDirectory: stats.isDirectory()})
+
+    }
+    res.json(resData) 
+   } catch (error) { 
+    res.json({error: error.message})
+   }
+})
+
