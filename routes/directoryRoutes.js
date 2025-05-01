d15458d1074dcec9429da2f3a2cb771aa8225a7f
vivek prajapati <vivek011prajapati@gmail.com>
2025-05-01 13:27:29 +0530

    setting up routes for renaming files and directory

--- routes/directoryRoutes.js
+++ routes/directoryRoutes.js
@@ -6,7 +6,7 @@ import path from "path"
 
 const app = express()
 
-app.post("/directory/?*", async (res,req) => {
+app.post("/?*", async (res,req) => {
     const dirname = path.join('/', req.params[0]) 
     try {
         await mkdir(`./storage/${dirname}`);
